6c84bf1d5155def209d392e6cf959d117e4ab50e
yaerei <yae.tetsuo@gmail.com>
2018-07-25 10:52:15 +0900

    Initial commit

---
+++ app/src/main/java/reduce/project/yaerei/listview_input/MainActivity.java
@@ -0,0 +1,47 @@
+package reduce.project.yaerei.listview_input;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListActivity list = new ListActivity();
+    ListView listview;
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        adapter = new ArrayAdapter(this,android.R.layout.simple_expandable_list_item_1);
+
+        listview = (ListView)findViewById(R.id.listview);
+
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final ArrayAdapter ad = (ArrayAdapter)listview.getAdapter();
+
+                final String item = (String)ad.getItem(i);
+
+
+            }
+        });
+
+        list.Listinput("高尾",adapter);
+        list.Listdisplay(adapter);
+
+    }
+
+    public void input(View v){
+        list.Listinput("高幡不動",adapter);
+
+    }
+}
